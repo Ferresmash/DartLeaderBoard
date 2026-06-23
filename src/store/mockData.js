4a3a6a5a1d7478c922c455b7d9671b4da01a66f5
Ferdinand Öhrn <ferdinand.ohrn@cloudme.com>
2026-06-23 20:49:45 +0200

    added galten and lunkan
    (cherry picked from commit cd7084e4e99e0ea283d4bbf5bdc27b77fe78894a)

--- src/store/mockData.js
+++ src/store/mockData.js
@@ -2,12 +2,16 @@ import ferdinandImg from './img/ferdinand.png';
 import maxImg from './img/max.png';
 import emilImg from './img/emil.png';
 import tedImg from './img/ted.png';
+import galtenImg from './img/galten.png';
+import lunkanImg from './img/lunkan.png';
 
 export const initialPlayers = [
   { id: 'ferdinand', name: 'Ferdinand Öhrn', totalWins: 0, gamesPlayed: 0, bestScore: 0, pfpUrl: ferdinandImg },
   { id: 'max', name: 'Max Sundberg', totalWins: 0, gamesPlayed: 0, bestScore: 0, pfpUrl: maxImg },
   { id: 'emil', name: 'Emil Holm', totalWins: 0, gamesPlayed: 0, bestScore: 0, pfpUrl: emilImg },
-  { id: 'ted', name: 'Ted Boman', totalWins: 0, gamesPlayed: 0, bestScore: 0, pfpUrl: tedImg }
+  { id: 'ted', name: 'Ted Boman', totalWins: 0, gamesPlayed: 0, bestScore: 0, pfpUrl: tedImg },
+  { id: 'galten', name: 'Galten', totalWins: 0, gamesPlayed: 0, bestScore: 0, pfpUrl: galtenImg },
+  { id: 'lunkan', name: 'Lunkan', totalWins: 0, gamesPlayed: 0, bestScore: 0, pfpUrl: lunkanImg }
 ];
 
 // Initialize empty deployment match history constraint.
